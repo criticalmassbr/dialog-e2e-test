9b8ae95abf436b58fc7770e1bbe4433c9a8ea9af
mateus-f-torres <mateusfigtorres@gmail.com>
2020-08-27 21:49:22 -0300

    test: write initial load and search scripts

--- __tests__/demo.spec.js
+++ __tests__/demo.spec.js
@@ -1,12 +1,28 @@
 // import 'expect-puppeteer';
+const timeout = 10000
 
 beforeAll(async () => {
-  await page.goto(URL, {waitUntil: 'domcontentloaded'});
+  await page.goto(URL, {waitUntil: 'networkidle0'});
 })
 
 describe('Inital failing spec', () => {
   test('Title of the page', async () => {
     const title = await page.title()
-    expect(title).toMatch('foobar');
+    expect(title).toMatch('Google Play');
   })
+
+  test('Fill search', async () => {
+    const input = await page.waitForSelector('form[name="gbqf"] input[name="q"]')
+    await input.focus()
+    await input.type('pepapp', {delay: 100})
+    await input.press('Enter')
+  })
+
+  /*
+  test('Click app', async () => {
+    // const [targetElement] = await page.$x('//a[contain]')
+    const div = await page.waitForSelector('div[title*="PEPapp"]').parentNode
+    await div.parentNode.click()
+  }, timeout)
+  */
 })
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js']
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  testMatch: ['<rootDir>/__tests__/*.spec.js']
 }
