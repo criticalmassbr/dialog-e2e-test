fbfe9f02a5ad21c5c0924af256ef93c8636a7c9f
mateus-f-torres <mateusfigtorres@gmail.com>
2020-08-27 22:50:00 -0300

    test: write app click and read more specs

--- __tests__/demo.spec.js
+++ __tests__/demo.spec.js
@@ -1,28 +1,32 @@
 // import 'expect-puppeteer';
-const timeout = 10000
+const timer = 10000
 
 beforeAll(async () => {
   await page.goto(URL, {waitUntil: 'networkidle0'});
-})
+}, timer)
 
 describe('Inital failing spec', () => {
   test('Title of the page', async () => {
     const title = await page.title()
     expect(title).toMatch('Google Play');
-  })
+  }, timer)
 
   test('Fill search', async () => {
     const input = await page.waitForSelector('form[name="gbqf"] input[name="q"]')
     await input.focus()
     await input.type('pepapp', {delay: 100})
     await input.press('Enter')
-  })
+  }, timer)
 
-  /*
   test('Click app', async () => {
-    // const [targetElement] = await page.$x('//a[contain]')
-    const div = await page.waitForSelector('div[title*="PEPapp"]').parentNode
-    await div.parentNode.click()
-  }, timeout)
-  */
+    const div = await page.waitForSelector('div[title*="PEPapp"]')
+    await div.click()
+  }, timer)
+
+  test('Click read more', async () => {
+    const btn = await page.waitForSelector('div[jsname="xBmnf"]')
+    await btn.click()
+    await page.waitFor(500)
+  }, timer)
+   
 })
